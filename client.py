67c563536cc0cef71c647868ec1a78896d01d145
DuckyLuckOO <duckyluck@tutamail.com>
2025-11-12 22:33:40 +0100

    · Update n1
    1- Updated server.py and client.py to allow listening and writing with multithreading

--- client.py
+++ client.py
@@ -1,9 +1,57 @@
 import socket
+import threading
+import sys
 
 s = socket.socket()
 s.connect(("127.0.0.1", 5000))
 
-s.sendall(b"Hello server!")
-print(s.recv(1024).decode())
-s.close()
+RED     = "\033[91m"
+GREEN   = "\033[92m"
+YELLOW  = "\033[93m"
+BLUE    = "\033[94m"
+MAGENTA = "\033[95m"
+CYAN    = "\033[96m"
+WHITE   = "\033[97m"
+RESET   = "\033[0m"
+
+logo = rf"""
+{YELLOW}
+  _____       _ _          _____ _           _   
+ / ____|     (_) |        / ____| |         | |  
+| (___  _ __  _| | _____ | |    | |__   __ _| |_ 
+ \___ \| '_ \| | |/ / _ \| |    | '_ \ / _` | __|
+ ____) | |_) | |   <  __/| |____| | | | (_| | |_ 
+|_____/| .__/|_|_|\_\___| \_____|_| |_|\__,_|\__|
+       | |                                              
+       |_|          {CYAN}💬  SpikeChat Connected  💬{RESET}
+"""
+print(logo + RESET)
+
+def recv_loop(sock: socket.socket):
+    try:
+        while True:
+            message = sock.recv(1024).decode()
+            print(message)
+    except:
+        print("recv error")
+    finally:
+        sock.close()
+
+
+def interactive_chat(sock: socket.socket):
+    t = threading.Thread(target=recv_loop, args=(s,), daemon=True)
+    t.start()
+    try:    
+        while True:
+            message = input(" ---- ") 
+            sock.sendall(message.encode())
+    except(KeyboardInterrupt, EOFError):
+        try: 
+            sock.shutdown(socket.SHUT_WR)
+        except OSError:
+            pass
+    finally:
+        t.join(timeout=1)
+
+interactive_chat(s)
 
--- server.py
+++ server.py
@@ -6,27 +6,49 @@ s = socket.socket()
 s.bind(("0.0.0.0", 5000))
 s.listen(1)
 print("Server ready on port 5000...")
-
-
+user_ip = ""
 
 while True:
     conn, addr = s.accept()
-    ip, port = addr
-    add_user(str(ip))
+    user_ip, port = addr
+    add_user(str(user_ip))
     
-    if ip == "127.0.0.1":
-        break
-    else:
-        print("Connection tried by " + ip + ":" + port)
-        conn.sendall(b"You snicky spike, ain't no way you can spike our spike")
-        conn.close()
     
-print("Connected by", addr)
+    # if user_ip == "127.0.0.1":
+    #     break
+    # else:
+    #     print("Connection tried by " + user_ip + ":" + port)
+    #     conn.sendall(b"You snicky spike, ain't no way you can spike our spike")
+    #     conn.close()
+    break
+
+RED     = "\033[91m"
+GREEN   = "\033[92m"
+YELLOW  = "\033[93m"
+BLUE    = "\033[94m"
+MAGENTA = "\033[95m"
+CYAN    = "\033[96m"
+WHITE   = "\033[97m"
+RESET   = "\033[0m"
+
+logo = rf"""
+{YELLOW}
+  _____       _ _          _____ _           _   
+ / ____|     (_) |        / ____| |         | |  
+| (___  _ __  _| | _____ | |    | |__   __ _| |_ 
+ \___ \| '_ \| | |/ / _ \| |    | '_ \ / _` | __|
+ ____) | |_) | |   <  __/| |____| | | | (_| | |_ 
+|_____/| .__/|_|_|\_\___| \_____|_| |_|\__,_|\__|
+       | |                                              
+       |_|          {CYAN}💬  SpikeChat Connected  💬{RESET}
+"""
+print(logo + RESET)
+
 
 while True:
     data = conn.recv(1024)
-    if not data:
-        break
-    print("Client:", data.decode())
-    conn.sendall(b"Message received!")
-    s.close()
+    if data:
+        message = user_ip + " : " + data.decode()
+        print("Client:", data.decode())
+        conn.sendall(message.encode())
+        
